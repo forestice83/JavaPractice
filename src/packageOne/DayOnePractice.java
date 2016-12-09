af2ad878d7db6553fd5c91cb05d7f81464f7a5a8
Md N Chowdhury <forestice831@gmail.com>
2016-12-09 16:15:44 -0500

    add new variable and Print statement

--- src/packageOne/DayOnePractice.java
+++ src/packageOne/DayOnePractice.java
@@ -3,6 +3,9 @@ package packageOne;
 public class DayOnePractice {
 
 	public static void main(String[] args) {
+		String name = "Forest ice Bron in ";
+		String dov = "Septembor 20Th 1983";
+		System.out.println(name + dov);
 		
 	}
 
