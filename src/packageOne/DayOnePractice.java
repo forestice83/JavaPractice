c27cbf3f5f36d084461e52b5e0b67bf0ba6717cc
Md N Chowdhury <forestice831@gmail.com>
2016-12-15 14:11:38 -0500

    Change data folder

--- src/packageOne/DayOnePractice.java
+++ src/packageOne/DayOnePractice.java
@@ -4,7 +4,7 @@ public class DayOnePractice {
 
 	public static void main(String[] args) {
 		String name = "Forest ice Bron in ";
-		String dov = "Septembor 20Th 1983";
+		String dov = "Septembor 20Th 1983.";
 		System.out.println(name + dov);
 		
 	}
